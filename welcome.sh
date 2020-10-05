a1aa0023b3483eca23866e5daacdfafbe6cf56aa
2324-sudo <nel530@my.utsa.edu>
2020-10-04 22:29:01 -0500

    welcome

---
+++ welcome.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+greeting="welcome"
+user=$(whoami)
+day=$(date +%A)
+
+
+echo "$greeting back $User! Today is $day, which is the best day of the entire week!"
+echo "Your Bash shell version is : $Bash_version. Enjoy!"
